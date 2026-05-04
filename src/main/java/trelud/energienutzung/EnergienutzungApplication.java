3d531d4bd6cbc7ce70151eeb39159888f621119e
trelud22 <trelud22@htl-kaindorf.at>
2026-05-04 08:06:37 +0200

    added Data

--- src/main/java/trelud/energienutzung/EnergienutzungApplication.java
+++ src/main/java/trelud/energienutzung/EnergienutzungApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EnergienutzungApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EnergienutzungApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EnergienutzungApplication.class, args);
+    }
 
 }
--- src/test/java/trelud/energienutzung/EnergienutzungApplicationTests.java
+++ src/test/java/trelud/energienutzung/EnergienutzungApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EnergienutzungApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
