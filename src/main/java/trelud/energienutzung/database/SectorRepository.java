7a1d19213f49df45a1ae30e5a38b7956e52457f2
grujud22 <grujud22@htl-kaindorf.at>
2026-06-19 11:06:00 +0200

    added query to find sectors by name

--- src/main/java/trelud/energienutzung/database/SectorRepository.java
+++ src/main/java/trelud/energienutzung/database/SectorRepository.java
@@ -1,9 +1,16 @@
 package trelud.energienutzung.database;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import trelud.energienutzung.pojo.Region;
 import trelud.energienutzung.pojo.Sector;
 
 @Repository
 public interface SectorRepository extends JpaRepository<Sector, Long> {
+    @Query("SELECT s FROM Sector s WHERE s.sectorName = :sectorName")
+    Sector getSectorByName(
+            @Param("sectorName") String sectorName
+    );
 }
